2b6dc4cf9b56f0debd3eabf202734af55303c2a4
soumya <soumya20134@iiitd.ac.in>
2024-04-13 02:57:29 +0530

    The mapper logic

--- Mapper.py
+++ Mapper.py
@@ -1,29 +1,51 @@
+import math
+import json
+
 class Mapper:
-    data_points = []
+    data_points = [1,10] # get from master
+    centroids = {1:[0.4, 7.2], 2:[0.8, 9.8], 3:[-1.5, 7.3],4:[8.1, 3.4]}
+    
+    with open("points.txt", "r") as file:
+        input_data = [list(map(float, line.strip().split(','))) for line in file]
 
-    data = [] # load
+    data = input_data[data_points[0]:data_points[1]] # load
 
-    centroids = []
+    print(data)
+    output = [] 
+    # [[cid,x,y]]
 
-
-    output = {}
-
-    def dist():
-        pass
+    def dist(self,p1,p2):
+        return math.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2))
 
     def maps(self):
         # for every data point calculate distance from each centroid
         # append lowest distance centroid : point
-
         for point in self.data:
+            min_dist = math.inf
+            mapped_c = None
             for c in self.centroids:
-                self.dist()
+                dist = min(self.dist(point,self.centroids[c]),min_dist)
+                print(dist)
+                if(dist < min_dist):
+                    min_dist = dist
+                    mapped_c = c
+            
+            point.insert(0, mapped_c)
+            self.output.append(point)
+            print(self.output)
+            self.partition()
 
-        pass
+    def partition(self):
+        partitions = {}
+        for c in self.centroids:
+            partitions[c] = []
+        for point in self.output:
+            cid = point[0]  # Get centroid ID
+            partitions[cid].append(point[1:])  # Append point without the centroid ID
+        for cid, points in partitions.items():
+            partition_file = f"partition_{cid}.json"
+            with open(partition_file, "w") as file:
+                json.dump(points, file)
 
-
-    def partition(self, output):
-        # iterate through dictionary
-        # make k different dictionaries
-        # dump in files
-        pass
+mapper = Mapper()
+mapper.maps()
